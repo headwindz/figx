95f8fd84cc5fe96891b18076976517e40a860d84
n0ruSh <michael2ib1989@gmail.com>
2022-01-08 01:22:16 +0800

    feat: export methods

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 // Interfaces
 export { NodeTypeEnum, ColorFormat, ColorModel, IColor } from './interface';
 
+// Node
+export { default as isTextNode } from './isTextNode';
+export { default as isFrameNode } from './isFrameNode';
+export { default as isPageNode } from './isPageNode';
+export { default as isVisible } from './isVisible';
+export { default as isFillableNode } from './isFillableNode';
+
 // Color
 export { default as toRgb } from './toRgb';
 export { default as toHsl } from './toHsl';
 
-// Node
-export { default as isTextNode } from './isTextNode';
-export { default as isFrameNode } from './isFrameNode';
-
 // Paint
 export { default as toSolidPaint } from './toSolidPaint';
+
+// Storage
+export { default as getPluginData } from './getPluginData';
+export { default as setPluginData } from './setPluginData';
 
 // Others
 export { default as getRandomId } from './getRandomId';
