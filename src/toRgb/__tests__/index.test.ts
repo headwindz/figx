0a5d7dd7db965e5b3cd4ebc93b5661e5f856dce1
n0ruSh <michael2ib1989@gmail.com>
2022-01-08 23:41:35 +0800

    tests: add rgba test for toRgb method

--- src/toRgb/__tests__/index.test.ts
+++ src/toRgb/__tests__/index.test.ts
@@ -3,15 +3,18 @@ import { ColorFormat } from '../../interface';
 
 const hex = '#FF0000';
 const hsl = 'hsl(0, 100%, 50%)';
+const rgba = { r: 255, g: 0, b: 0, alpha: 0.3 };
 
 describe('toRgb', () => {
   it('should work for rgb string', () => {
     expect(toRgb(hex, ColorFormat.STRING)).toEqual('rgb(255, 0, 0)');
+    expect(toRgb(rgba, ColorFormat.STRING)).toEqual('rgba(255, 0, 0, 0.3)');
     expect(toRgb(hsl, ColorFormat.STRING)).toEqual('rgb(255, 0, 0)');
   });
 
   it('should work for rgb array', () => {
     expect(toRgb(hex, ColorFormat.ARRAY)).toEqual([255, 0, 0]);
+    expect(toRgb(rgba, ColorFormat.ARRAY)).toEqual([255, 0, 0, 0.3]);
     expect(toRgb(hsl, ColorFormat.ARRAY)).toEqual([255, 0, 0]);
   });
 
@@ -20,6 +23,12 @@ describe('toRgb', () => {
       r: 255,
       g: 0,
       b: 0,
+    });
+    expect(toRgb(rgba, ColorFormat.OBJECT)).toEqual({
+      r: 255,
+      g: 0,
+      b: 0,
+      alpha: 0.3,
     });
     expect(toRgb(hsl, ColorFormat.OBJECT)).toEqual({
       r: 255,
