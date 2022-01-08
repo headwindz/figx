e485ce4a510566bf87eda36f2cd43802cddda5b7
n0ruSh <michael2ib1989@gmail.com>
2022-01-08 23:43:46 +0800

    refactor: remove getColorMethodName

--- src/_internal/toColor.ts
+++ src/_internal/toColor.ts
@@ -1,10 +1,9 @@
 import { IColor, ColorModel, ColorFormat } from '../interface';
-import getColorMethodName from './getColorMethodName';
 import Color from 'color';
 
 export default function toColor(colorModel: ColorModel) {
   return (color: IColor, format: ColorFormat = ColorFormat.STRING) => {
-    const methodName = getColorMethodName(format);
+    const methodName = format;
     const theColor = Color(color)[colorModel]() as Color;
     return theColor[methodName]();
   };
