7786af5e5dc67ccc4ea8d5b0bfe4c7fc2a48f928
n0ruSh <michael2ib1989@gmail.com>
2022-02-02 12:59:46 +0800

    feat: add window.copy as a backup for execCommand copy

--- src/copyToClipboard/__tests__/index.test.ts
+++ src/copyToClipboard/__tests__/index.test.ts
@@ -2,7 +2,7 @@ import copyToClipboard from '../index';
 
 describe('copyToClipboard', () => {
   it('should copy to clipboard', () => {
-    document.execCommand = jest.fn();
+    document.execCommand = jest.fn(() => true);
     copyToClipboard('Copied');
     expect(document.execCommand).toHaveBeenCalledWith('copy');
   });
--- src/copyToClipboard/index.ts
+++ src/copyToClipboard/index.ts
@@ -1,8 +1,23 @@
 export default function copyToClipboard(value: string) {
-  const area = document.createElement('textarea');
-  document.body.appendChild(area);
-  area.value = value;
-  area.select();
-  document.execCommand('copy');
-  document.body.removeChild(area);
+  if (window.copy) {
+    window.copy(value);
+    return true;
+  } else {
+    try {
+      const area = document.createElement('textarea');
+      document.body.appendChild(area);
+      area.value = value;
+      area.focus();
+      area.select();
+      const result = document.execCommand('copy');
+      document.body.removeChild(area);
+      if (!result) {
+        throw new Error();
+      }
+      return true;
+    } catch (e) {
+      console.error(`Unable to copy the value: ${value}`);
+      return false;
+    }
+  }
 }
